80e940ab2f3b6f4142b00985530f57465c3a96b3
Izzur Zuhri <izzur.zuhri@gmail.com>
2020-06-03 17:59:09 +0700

    Update index content

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,22 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Izzur" />
     <h1>Hi people</h1>
-    <h2>Personal Info</h2>
     <h2>Work Experience</h2>
+    <ul>
+      <li>Sinarmas Mining</li>
+    </ul>
     <h2>Education</h2>
-    <h2>Certification</h2>
+    <ul>
+      <li>Institut Teknologi Sepuluh Nopember</li>
+    </ul>
     <h2>Skills</h2>
+    <ul>
+      <li>Programming Language<br></br>NodeJS, PHP, Java, Kotlin, Dart</li>
+      <li>Framework<br></br>ExpressJS, Laravel, CodeIgniter, Flutter</li>
+      <li>Database<br></br>MySQL/MariaDB, PostgreSQL, MongoDB</li>
+      <li>Cloud<br></br>AWS, Google Cloud, Microsoft Azure, Heroku</li>
+      <li>Web Technology<br></br>REST API, PWA, Kubernetes, Jamstack</li>
+    </ul>
   </Layout>
 )
 
